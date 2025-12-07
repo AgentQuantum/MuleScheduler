751d77229bff78836fac906f64d4bb3549937328
Rish C <rishitchatterjee007@gmail.com>
2025-12-07 07:15:50 -0500

    Enable demo mode in vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Enable demo mode in production builds
+    'import.meta.env.VITE_DEMO_MODE': JSON.stringify('true'),
+  },
   server: {
     port: 5173,
     proxy: {
